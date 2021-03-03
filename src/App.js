5c1b407ab98b21319b89d599f25fbf6e2fe12b1a
Kristian Andersen <krilleandersen@gmail.com>
2021-03-03 09:18:08 +0100

    Added: InputBar with font-awesome icons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import {faArrowAltCircleRight, faKeyboard } from '@fortawesome/free-solid-svg-icons'
 
 import Appheader from './components/AppHeader'
+import Inputbar from './components/InputBar'
 
+library.add(fab,far, faArrowAltCircleRight, faKeyboard)
 function App() {
   return (
+    <div>
         <Appheader/>
+        <Inputbar/>
+        </div>
   );
 }
 
---
+++ src/components/InputBar.js
@@ -0,0 +1,26 @@
+import React from "react";
+import '../App.css';
+import './css/InputBar.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+
+const InputBar = () => {
+    return(  
+    <div>
+     <main>
+        <div className="inputwrap">
+                <div className="inputwhitewrap">
+                    <div className="formwrap">
+                    <div className="form-input ">
+                        <label htmlFor="input" className="iconl"><FontAwesomeIcon className="far fa-keyboard" icon='keyboard'/></label>
+                        <div className="iconr"><FontAwesomeIcon className="fas fon" icon='arrow-alt-circle-right'/></div>
+                        <input id="input" type="text" className="input" placeholder="What's your name?"/>
+                      </div>
+                    </div>  
+                </div>
+        </div>
+    </main>
+   </div>);
+   };
+
+   export default InputBar;
