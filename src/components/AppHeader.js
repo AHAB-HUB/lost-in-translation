843a0a005c6ed83dfafaa4d440c2b94f67b3b3c3
Kristian Andersen <krilleandersen@gmail.com>
2021-03-03 08:38:38 +0100

    Added: AppHeader component

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,7 +1,24 @@
 import React from "react";
- 
+import '../App.css';
+import './css/AppHeader.css'
+import Logo from './images/Logo.png'
 const Appheader = () => {
- return <h1>Hello, world</h1>;
+ return(  
+ <div>
+ <header>
+    <span className="miniHeader">Lost in translation</span>
+    <hr className="divider" />
+    <div className="lhwrap">
+        <div className="logowrap">
+            <img src={Logo} className="logoimg" alt="lost-in-translation logo"/>
+        </div>
+        <div className="hwrap">
+            <h1>Lost in translation</h1><br/>
+            <h2>Get startet</h2>
+        </div>
+    </div>
+</header>
+</div>);
 };
 
 export default Appheader;
