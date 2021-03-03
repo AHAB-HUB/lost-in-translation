555d42e98dd08fc72c4321407b493adb236ff3c1
Kristian Andersen <krilleandersen@gmail.com>
2021-03-03 08:09:40 +0100

    Added: .env file so the project can run on mac

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Appheader from './components/AppHeader'
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          <Appheader/>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
