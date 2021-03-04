052bbce6933f06cdb263080f78747afaf1785c21
Ahmad <ahmadabdulal@outlook.com>
2021-03-04 15:32:15 +0100

    feat: added routing

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,28 @@ import Header from './shared/Header'
 import Footer from './shared/Footer'
 import Login from './containers/Login'
 import Translate from './containers/Translate'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { useState } from 'react'
 
 function App() {
+  const [username, setUsername] = useState(null)
+
+  const updateUsername = (name) => {
+    setUsername(name)
+  }
+
   return (
     <div className="app">
-      <Header />
-      <Login />
-      {/* <Translate /> */}
-      <Footer />
+      <Router>
+        <Header username={username} />
+
+        <Switch>
+          <Route path="/translate" component={Translate} />
+          <Route exact path="/" ><Login updateUsername={updateUsername} /></Route>
+        </Switch>
+
+        <Footer />
+      </Router>
     </div>
   );
 }
