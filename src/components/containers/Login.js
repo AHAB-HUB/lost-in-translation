abc94e376be3f36d3a07f230257a820c356cbb45
Ahmad <ahmadabdulal@outlook.com>
2021-03-05 12:50:06 +0100

    fix(login.js): prevent user from using whitespace as a username

--- src/components/containers/Login.js
+++ src/components/containers/Login.js
@@ -1,15 +1,23 @@
+import { useRef, useState } from 'react'
+import { useHistory } from 'react-router-dom'
+
 import '../../css/login.css'
 import logo from '../../assets/images/Logo.png'
 import splash from '../../assets/Splash.svg'
-import { useRef } from 'react'
-import { useHistory } from 'react-router-dom'
 import profile from '../../utils/user'
 
 const Login = ({ updateUsername }) => {
     const history = useHistory()
+    const [error, setError] = useState(null)
     const input = useRef(null)
 
     const onClickLogin = () => {
+
+        if (input.current.value.replaceAll(/\s/g, "").length < 1) {
+            setError("Enter a name first!")
+            return;
+        }
+
         updateUsername(input.current.value)
         //save user name to local storage
         const user = profile(input.current.value)
@@ -23,7 +31,6 @@ const Login = ({ updateUsername }) => {
             <link rel="preconnect" href="https://fonts.gstatic.com" />
             <link href="https://fonts.googleapis.com/css2?family=Sanchez&display=swap" rel="stylesheet" />
             <div className="banner">
-
                 <div className="logo-container">
                     <img className="splash" src={splash} alt="splash" />
                     <img className="logo" src={logo} alt="logo" />
@@ -42,6 +49,7 @@ const Login = ({ updateUsername }) => {
                         <input ref={input} type="text" placeholder="What's your name?" required />
                         <button type="button" className="btn" onClick={onClickLogin}>login</button>
                     </div>
+                    {error && <label className="error-message">{error}</label>}
                 </div>
             </div>
         </div>
