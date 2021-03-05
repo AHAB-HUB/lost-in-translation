8abd545757935d7709875b3abee3fe7bc6862ecb
Ahmad <ahmadabdulal@outlook.com>
2021-03-05 17:38:05 +0100

    feat(App.js): check local storage if the user is already logged in on start up

--- src/components/App.js
+++ src/components/App.js
@@ -6,18 +6,35 @@ import Translate from './containers/Translate'
 import Profile from './containers/Profile'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import PrivateRoute from './PrivateRoute';
+import { useDispatch } from 'react-redux'
+import { setUsername } from '../redux/actions/usernameActions'
+import { useEffect } from 'react'
+
+import profile from '../utils/user'
 
 function App() {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+
+    if (localStorage.getItem("data") !== null) {
+      let user = profile()
+      user.setUser(JSON.parse(localStorage.getItem("data")))
+
+      dispatch({ type: 'SIGN_IN' })
+      dispatch(setUsername(user.getUsername()))
+    }
+  }, [dispatch])
 
   return (
     <div className="app">
       <Router>
         <Header />
-
         <Switch>
-          <PrivateRoute exact path="/translate" component={Translate} />
-          <PrivateRoute exact path="/profile" component={Profile} />
-          <Route exact path="/signin" component={Login} />
+          <Route path="/" component={Login} exact />
+          <Route path="/signin" component={Login} exact />
+          <PrivateRoute path="/profile" component={Profile} exact />
+          <PrivateRoute path="/translate" component={Translate} exact />
         </Switch>
 
         <Footer />
