2f66f6081efa55d7ef19285b9009ce13511645bd
Ahmad <ahmadabdulal@outlook.com>
2021-03-05 15:20:51 +0100

    feat(header.js): added a dropdown list to log out or navigate to the profile

--- src/components/shared/Header.js
+++ src/components/shared/Header.js
@@ -2,6 +2,7 @@ import '../../css/App.css';
 import '../../css/header.css';
 import profile from '../../assets/images/profile.jpg';
 import { useHistory } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const Header = ({ username }) => {
     const history = useHistory()
@@ -10,10 +11,17 @@ const Header = ({ username }) => {
         history.push('/profile')
     }
 
+    const logout = () => {
+        localStorage.removeItem("data")
+        history.push('/')
+    }
+
     return (
         <div className="header">
             <link rel="preconnect" href="https://fonts.gstatic.com" />
             <link href="https://fonts.googleapis.com/css2?family=Love+Ya+Like+A+Sister&display=swap" rel="stylesheet" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" />
+            <link href="https://fonts.googleapis.com/css2?family=Sanchez&display=swap" rel="stylesheet" />
             <p>Lost in translation</p>
             {username && <div className="image-container">
                 <div className="item">
@@ -21,6 +29,10 @@ const Header = ({ username }) => {
                 </div>
                 <div className="item" onClick={redirectToProfile}>
                     <img className="image" src={profile} alt={username} />
+                </div>
+                <div className="dropdown-container">
+                    <Link className="dropdown-item" to="/profile">Profile</Link>
+                    <div className="dropdown-item" onClick={logout}>Log Out</div>
                 </div>
             </div>}
         </div>
