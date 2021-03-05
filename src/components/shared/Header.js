b7bd58613290d3d582e4261076a2ae8011fe1638
Ahmad <ahmadabdulal@outlook.com>
2021-03-05 16:41:25 +0100

    feat(header.js): username and islogged Boolean are now updated from redux store

--- src/components/shared/Header.js
+++ src/components/shared/Header.js
@@ -3,15 +3,20 @@ import '../../css/header.css';
 import profile from '../../assets/images/profile.jpg';
 import { useHistory } from 'react-router-dom'
 import { Link } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
 
-const Header = ({ username }) => {
+const Header = () => {
     const history = useHistory()
+    const username = useSelector(state => state.username)
+    const isLogged = useSelector(state => state.isLogged)
+    const dispatch = useDispatch()
 
     const redirectToProfile = () => {
         history.push('/profile')
     }
 
     const logout = () => {
+        dispatch({ type: "SIGN_IN" })
         localStorage.removeItem("data")
         history.push('/')
     }
@@ -22,8 +27,9 @@ const Header = ({ username }) => {
             <link href="https://fonts.googleapis.com/css2?family=Love+Ya+Like+A+Sister&display=swap" rel="stylesheet" />
             <link rel="preconnect" href="https://fonts.gstatic.com" />
             <link href="https://fonts.googleapis.com/css2?family=Sanchez&display=swap" rel="stylesheet" />
+
             <p>Lost in translation</p>
-            {username && <div className="image-container">
+            {isLogged && <div className="image-container">
                 <div className="item">
                     <p className="nametag">{username}</p>
                 </div>
